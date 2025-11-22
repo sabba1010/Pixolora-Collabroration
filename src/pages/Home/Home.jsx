5ccf859a141bd600ccf5f5c542eda873cf74da40
Sabba Hossain <sabbahossain@gmail.com>
2025-11-22 09:54:07 +0600

    new

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import Hero from './Hero';
-import OceanFreigh from './OceanFreigh';
-import Transporte from './Transporte';
-import Form from './Form';
-import Conocenos from './Conocenos';
-import Packaging from './Packaging';
-import Testimonials from './testimonials';
-import HomContactText from './HomContactText';
-import CompleteFreight from './CompleteFreight';
-import SLiderMarque from './SLiderMarque';
-
 
 const Home = () => {
     return (
         <div>
-            <Hero/>
-            <OceanFreigh/>
-            <Transporte/>
-            <SLiderMarque/>
-            <Form/>
-            <Conocenos/>
-            <Packaging/>
-            <CompleteFreight/>
-            <Testimonials/>
-            <HomContactText/>
+            
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -13,4 +17,7 @@ export default {
     },
   },
   plugins: [],
-}
+};
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
